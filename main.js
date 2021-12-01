5b169c225ca362bbd4ceb736920c37a3356e1b35
ThatCoderKid726 <83380065+ThatCoderKid726@users.noreply.github.com>
2021-11-30 21:18:08 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -31,3 +31,63 @@ function speak() {
     synth.speak(utterThis);
 }
 
+function check(){ 
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "good")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "stop")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9995;";
+        }
+        if(results[0].label == "peace")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        
+        if(results[0].label == "ok")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+
+        if(results[0].label == "hello")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128400;";
+        }
+
+        if(results[1].label == "good")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128077;";
+        }
+        if(results[1].label == "stop")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#9995;";
+        }
+        if(results[1].label == "peace")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#9996;";
+        }
+        if(results[1].label == "ok")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128076;";
+        }
+        if(results[1].label == "hello")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128400;";
+        }
+    }
+}
